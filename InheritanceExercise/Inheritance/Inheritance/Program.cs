13b565a7f9c2d7ab715a08c53d1d05070aa1009e
Daniel Cano <josecastilloc93@gmail.com>
2018-08-04 20:26:36 -0500

    Inheritance
    Implementation of inheritance in a class base to a sub class to another sub class.

--- InheritanceExercise/Inheritance/Inheritance/Program.cs
+++ InheritanceExercise/Inheritance/Inheritance/Program.cs
@@ -8,31 +8,32 @@ namespace Inheritance
 {
     // Inheritor implicitly inherits from the Object class.
     public class Inheritor
-    {     
-        private static int currentID;
+    {
+        // Static field stores the property1 of the last Inheritor that has been created.
+        private static int Data;
 
         //Properties.
-        protected int Data1 { get; set; }
-        protected string Title { get; set; }
-        protected string Description { get; set; }
-        protected TimeSpan jobLength { get; set; }
+        protected int Property1 { get; set; }
+        protected string Property2 { get; set; }
+        protected string Property3 { get; set; }
+        protected TimeSpan Property4 { get; set; }
 
         //Constructor.
         public Inheritor()
         {
-            Data1 = 0;
-            Title = "Default title";
-            Description = "Default description.";
-            jobLength = new TimeSpan();
+            Property1 = 0;
+            Property2 = "Default";
+            Property3 = "Default.";
+            Property4 = new TimeSpan();
         }
 
-        // Instance constructor that has three parameters.
-        public Inheritor(string title, string desc, TimeSpan joblen)
+        // Instance constructor - has three parameters.
+        public Inheritor(string p2, string p3, TimeSpan p4)
         {
-            this.Data1 = GetNextID();
-            this.Title = title;
-            this.Description = desc;
-            this.jobLength = joblen;
+            this.Property1 = GetNext();
+            this.Property2 = p2;
+            this.Property3 = p3;
+            this.Property4 = p4;
         }
 
         // Static constructor to initialize the static member, currentID. This
@@ -40,58 +41,55 @@ namespace Inheritance
         // of WorkItem or ChangeRequest is created, or currentID is referenced.
         static Inheritor()
         {
-            currentID = 0;
+            Data = 0;
         }
 
-        protected int GetNextID()
+        protected int GetNext()
         {
-            // currentID is a static field. It is incremented each time a new
-            // instance of WorkItem is created.
-            return ++currentID;
+            // Data is a static field. It is incremented each time a new instance of Inheritor is created.
+            return ++Data;
         }
 
-        // Method Update enables you to update the title and job length of an
-        // existing WorkItem object.
-        public void Update(string title, TimeSpan joblen)
+        // Method Update enables you to update the property2 and property4 length of an existing Inheritor object.
+        public void Update(string p2, TimeSpan p4)
         {
-            this.Title = title;
-            this.jobLength = joblen;
+            this.Property2 = p2;
+            this.Property4 = p4;
         }
 
-        // Virtual method override of the ToString method that is inherited
-        // from System.Object.
+        //( Virtual method override) of the( ToString method that is inheritedfrom System.Object).
         public override string ToString()
         {
-            return String.Format("{0} - {1}", this.Data1, this.Title);
+            return String.Format("{0} - {1}", this.Property1, this.Property2);
         }
     }
 
     public class Inheritor2 : Inheritor
     {
-        protected int originalItemID { get; set; }
+        protected int originalProperty1 { get; set; }
 
         // Constructors. Because neither constructor calls a base-class 
-        // constructor explicitly, the default constructor in the base class
-        // is called implicitly. The base class must contain a default 
-        // constructor.
 
+        // constructor explicitly, the default constructor in the base class is called implicitly. The base class must contain a default constructor.
+        
         // Default constructor for the derived class.
-        public Inheritor2() { }
+        public Inheritor2()
+        {
 
-        // Instance constructor that has four parameters.
-        public Inheritor2(string title, string desc, TimeSpan jobLen,
-                             int originalID)
+        }
+
+        // Instance constructor that has parameters.
+        public Inheritor2(string p2, string p3, TimeSpan p4, int op)
         {
             // The following properties and the GetNexID method are inherited 
             // from WorkItem.
-            this.Data1 = GetNextID();
-            this.Title = title;
-            this.Description = desc;
-            this.jobLength = jobLen;
+            this.Property1 = GetNext();
+            this.Property2 = p2;
+            this.Property3 = p3;
+            this.Property4 = p4;
 
-            // Property originalItemId is a member of ChangeRequest, but not 
-            // of WorkItem.
-            this.originalItemID = originalID;
+            // Property original is a member of Inheritor2, but not of Inheritor1.
+            this.originalProperty1 = op;
         }
     }
 
@@ -99,21 +97,19 @@ namespace Inheritance
         {
             static void Main(string[] args)
             {
-            // Create an instance of WorkItem by using the constructor in the 
-            // base class that takes three arguments.
-            Inheritor item = new Inheritor("Fix Bugs","Fix all bugs in my code branch", new TimeSpan(3, 4, 0, 0));
+            // Create an instance of Inheritor by using the constructor in the base class that takes three arguments.
+            Inheritor inheritor = new Inheritor("Parameter Hineritor","Parameter", new TimeSpan(3, 4, 0, 0));
             // Create an instance of ChangeRequest by using the constructor in
             // the derived class that takes four arguments.
-            Inheritor2 change = new Inheritor2("Change Base Class Design","Add members to the class",new TimeSpan(4, 0, 0),1);
-            // Use the ToString method defined in WorkItem.
-            Console.WriteLine(item.ToString());
-            // Use the inherited Update method to change the title of the 
-            // ChangeRequest object.
-            change.Update("Change the Design of the Base Class",new TimeSpan(4, 0, 0));      
-            // ChangeRequest inherits WorkItem's override of ToString.
-            Console.WriteLine(change.ToString());
+            Inheritor2 inheritor2 = new Inheritor2("Parameter","Parameter",new TimeSpan(4, 0, 0),1);
+            // Use the ToString method defined in Inheritor1.
+            Console.WriteLine(inheritor.ToString());
+            // Use the inherited Update method to change the title of the Inheritor2 object.
+            inheritor2.Update("Parameter Hineritor 2",new TimeSpan(4, 0, 0));      
+            // Inheritor2 inherits Inheritor override of ToString.
+            Console.WriteLine(inheritor2.ToString());
             // Keep the console open in debug mode.
-            Console.WriteLine("Press any key to exit.");
+            Console.WriteLine("Press key to exit.");
             Console.ReadKey();
         }
       }
